cfa6c1e4464e39f8de8caadae3acf6f01d597559
William F <60886603+wtcf90@users.noreply.github.com>
2020-06-12 23:18:51 -0700

    Update EmployeeSQL_Table_Schemata.sql

--- EmployeeSQL/EmployeeSQL_Table_Schemata.sql
+++ EmployeeSQL/EmployeeSQL_Table_Schemata.sql
@@ -2,7 +2,7 @@
 DROP TABLE titles CASCADE
 DROP TABLE employees CASCADE
 DROP TABLE salaries CASCADE
-DROP TABLE dept_manager CASCADE 
+DROP TABLE dept_manager CASCADE
 DROP TABLE departments CASCADE
 DROP TABLE dept_emp CASCADE
 
@@ -58,7 +58,7 @@ CREATE TABLE "dept_emp" (
     "dept_no" VARCHAR   NOT NULL
 );
 
---Adding Foreign Key Contraints, based on observations in ERD. 
+--Adding Foreign Key Contraints, based on observations in ERD.
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
@@ -76,3 +76,24 @@ REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
+
+--Importing CSV files data, into respective table
+--Ensuring data is imported in same order as table creation
+COPY titles FROM '/Users/williamforsyth/Documents/uc_davis/Homework_Repos/sql-challenge/data/titles.csv' DELIMITER ',' CSV HEADER;
+COPY employees FROM '/Users/williamforsyth/Documents/uc_davis/Homework_Repos/sql-challenge/data/employees.csv' DELIMITER ',' CSV HEADER;
+COPY salaries FROM '/Users/williamforsyth/Documents/uc_davis/Homework_Repos/sql-challenge/data/salaries.csv' DELIMITER ',' CSV HEADER;
+COPY departments FROM '/Users/williamforsyth/Documents/uc_davis/Homework_Repos/sql-challenge/data/departments.csv' DELIMITER ',' CSV HEADER;
+COPY dept_manager FROM '/Users/williamforsyth/Documents/uc_davis/Homework_Repos/sql-challenge/data/dept_manager.csv' DELIMITER ',' CSV HEADER;
+COPY dept_emp FROM '/Users/williamforsyth/Documents/uc_davis/Homework_Repos/sql-challenge/data/dept_emp.csv' DELIMITER ',' CSV HEADER;
+
+-- Query template to run 'just in case' I find incorrect data types
+ALTER TABLE table_name
+ALTER COLUMN column_name datatype;
+
+--Systematicaly checking tables, following insersion of respective csv files.
+SELECT * FROM titles
+SELECT * FROM employees
+SELECT * FROM salaries
+SELECT * FROM departments
+SELECT * FROM dept_manager
+SELECT * FROM dept_emp
